6cd9925bf0c87dc7a8c642a7f456c4294067213b
LaNMaSteR53 <tjt1980@gmail.com>
2014-10-02 23:05:10 -0400

    Refactored much of the core module. Added OUI lookup functionality.

--- alerts.py
+++ alerts.py
@@ -1,17 +1,17 @@
 from config import *
 
 '''
-Each module must receive the 'data' variable as a parameter. The 'data' variable
-is a dictionary consisting of the bssid, essid, and rssi. Each module must start
-with 'alert_' and have a matching variable in config.py for enabling/disabling.
-All module variables should be defined in config.py.
+Each module must receive **kwargs as a parameter. The kwargs variable is a dictionary
+consisting of all the data extracted from the probe request. Each module name must
+start with 'alert_' and have a matching variable in config.py for enabling/disabling.
+Configurable module options may be defined in config.py.
 '''
 
 from email.mime.text import MIMEText
 import smtplib
 
-def alert_sms(data):
-    msg = MIMEText('WUDS proximity alert! A foreign device (%s) has been detected on the premises.' % (data['bssid']))
+def alert_sms(**kwargs):
+    msg = MIMEText('WUDS proximity alert! A foreign device (%s - %s) has been detected on the premises.' % (kwargs['bssid'], kwargs['oui']))
     server = smtplib.SMTP(SMTP_SERVER)
     server.starttls()
     server.login(USERNAME, PASSWORD)
--- core.py
+++ core.py
@@ -1,20 +1,47 @@
-#!/usr/bin/env python
-
-# import python modules
 from datetime import datetime, timedelta
+import json
 import socket
 import struct
 import sys
+import traceback
+import urllib2
 
 # import wuds modules
 sys.dont_write_bytecode = True
 from config import *
 from alerts import *
 
+# define constants
+LOG_LEVELS = {
+    0: 'INFO',
+    1: 'ERROR',
+    2: 'ALERT',
+    }
+
 def log(level, message):
+    # wuds runs as root and should be able to write anywhere
     with open(LOG_FILE, 'a') as fp:
         fp.write('[%s] [%s] %s\n' % (str(datetime.now()), LOG_LEVELS[level], message))
         fp.flush()
+
+def oui_lookup(mac):
+    resp = urllib2.urlopen('https://www.macvendorlookup.com/api/v2/%s' % mac)
+    jsonobj = json.load(resp)
+    return jsonobj[0]['company']
+
+def call_alerts(**kwargs):
+    for var in globals():
+        # find config variables for alert modules
+        if var.startswith('ALERT_') and globals()[var] == True:
+            # dynamically call enabled alert modules
+            if var.lower() in globals():
+                func = globals()[var.lower()]
+                try:
+                    func(**kwargs)
+                    log(0, '%s alert triggered [%s]' % (var[6:], kwargs['bssid']))
+                except:
+                    traceback.print_exc()
+                    log(1, '%s alert failed [%s]' % (var[6:], kwargs['bssid']))
 
 def packet_handler(pkt):
     rtlen = struct.unpack('h', pkt[2:4])[0]
@@ -22,58 +49,41 @@ def packet_handler(pkt):
     stype = ord(pkt[rtlen]) >> 4
     # check if probe request frame
     if ftype == 0 and stype == 4:
+        # parse bssid
         bssid = pkt[36:42].encode('hex')
         bssid = ':'.join([bssid[x:x+2] for x in xrange(0, len(bssid), 2)])
-        # check whitelist for probing MAC address
+        # check whitelist for probing mac address
         if bssid not in MAC_LIST:
+            # parse rssi
             rssi = struct.unpack("b",pkt[:rtlen][-4:-3])[0]
             # check proximity
             if rssi > RSSI_THRESHOLD:
+                # parse essid
                 essid = pkt[52:52+ord(pkt[51])] if ord(pkt[51]) > 0 else '<None>'
-                log(2, 'Foreign probe detected. [MAC=%s RSSI=%d SSID=%s]' % (bssid, rssi, essid))
+                # get oui for bssid
+                if bssid not in ouis:
+                    try: ouis[bssid] = oui_lookup(bssid)
+                    except: ouis[bssid] = 'Unknown'
+                    log(0, 'OUI resolved [%s => %s]' % (bssid, ouis[bssid]))
+                oui = ouis[bssid]
+                log(2, 'Foreign probe detected [MAC=%s, RSSI=%d, SSID=%s, OUI=%s]' % (bssid, rssi, essid, oui))
+                # send alerts periodically
                 if bssid not in alerts:
                     alerts[bssid] = datetime.now() - timedelta(minutes=5)
-                # send alerts periodically
                 if (datetime.now() - alerts[bssid]).seconds > ALERT_THRESHOLD:
                     alerts[bssid] = datetime.now()
-                    for var in globals():
-                        # find config variables for alert modules
-                        if var.startswith('ALERT_') and globals()[var] == True:
-                            # dynamically call enabled alert modules
-                            if var.lower() in globals():
-                                func = globals()[var.lower()]
-                                func({'bssid': bssid, 'rssi': rssi, 'essid': essid})
-                            log(0, '%s alert triggered. [%s]' % (var[6:], bssid))
+                    call_alerts(bssid=bssid, rssi=rssi, essid=essid, oui=oui)
 
-# instatiate variables
+log(0, 'WUDS started')
+# setup the sniffer
+rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
+rawSocket.bind((IFACE, 0x0003))
 alerts = {}
-LOG_LEVELS = {
-    0: 'INFO',
-    1: 'ERROR',
-    2: 'ALERT',
-    }
-try:
-    log(0, 'WUDS started.')
-    # setup the sniffer
-    rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
-    rawSocket.bind((IFACE, 0x0003))
-    # load whitelisted MAC addresses
-    for mac in MAC_LIST:
-        log(0, 'MAC address whitelisted. [%s]' % (mac))
-    # start the sniffer
-    while True:
+ouis = {}
+# start the sniffer
+while True:
+    try:
         pkt = rawSocket.recvfrom(2048)[0]
         packet_handler(pkt)
-# must be first to prevent IOError/socket.error confusion
-# socket.error is seen as an IOError exception
-except socket.error as e:
-    print 'Interface error: %s (%s)' % (e.strerror, IFACE)
-    log(1, '%s (%s).' % (e.strerror, IFACE))
-# should be encountered first due to the initial log entry
-except IOError as e:
-    print 'Logging error: %s (%s)' % (e.strerror, LOG_FILE)
-    # hard exit to prevent additional call to log() on exit
-    sys.exit()
-except KeyboardInterrupt:
-    pass
-log(0, 'WUDS exited.')
+    except KeyboardInterrupt: break
+log(0, 'WUDS exited')

95d481cb0a999fe5ba007fe69a3afe2f7b1d05bf
LaNMaSteR53 <tjt1980@gmail.com>
2014-10-08 00:53:00 -0400

    Migrated logging from a flat file to a sqlite database and made the logging level configurable via the configration file.

--- config.py
+++ config.py
@@ -8,18 +8,26 @@ IFACE = 'mon0'
 
 # (LIST) List of MAC addresses expected within the premises
 MAC_LIST = [
-    'ee:ee:ee:ee:ee:ee',
-    'ff:ff:ff:ff:ff:ff',
+    'xx:xx:xx:xx:xx:xx',
+    'xx:xx:xx:xx:xx:xx',
     ]
 
-# (INT) RSSi threshold for triggering alerts
+# (INT) RSSI threshold for triggering alerts
 RSSI_THRESHOLD = -50
 
 # (INT) Number of seconds between alerts for persistent foreign probes
 ALERT_THRESHOLD = 120
 
-# (STR) Path to the log file
-LOG_FILE = '/var/log/wuds.log'
+# (STR) Path to the database file
+LOG_FILE = 'log.db'
+
+# (INT) Determines which probes are stored in the database
+# 0 = all probes
+# 1 = all foreign probes
+# 2 = all probes on the premises
+# 3 = all foreign probes on the premises
+# 4 = only probes that generate alerts
+LOG_LEVEL = 3
 
 #========
 # ALERTS
--- core.py
+++ core.py
@@ -1,6 +1,8 @@
+from contextlib import closing
 from datetime import datetime, timedelta
 import json
 import socket
+import sqlite3
 import struct
 import sys
 import traceback
@@ -12,22 +14,42 @@ from config import *
 from alerts import *
 
 # define constants
-LOG_LEVELS = {
+LOG_TYPES = {
+    0: 'messages',
+    1: 'probes',
+}
+MESSAGE_LEVELS = {
     0: 'INFO',
     1: 'ERROR',
     2: 'ALERT',
     }
 
-def log(level, message):
-    # wuds runs as root and should be able to write anywhere
-    with open(LOG_FILE, 'a') as fp:
-        fp.write('[%s] [%s] %s\n' % (str(datetime.now()), LOG_LEVELS[level], message))
-        fp.flush()
+def log(log_type, values):
+    values = (str(datetime.now()),) + values
+    values_str = ','.join('?'*len(values))
+    query = 'INSERT INTO %s VALUES (%s)' % (LOG_TYPES[log_type], values_str)
+    cur.execute(query, values)
+    conn.commit()
 
-def oui_lookup(mac):
-    resp = urllib2.urlopen('https://www.macvendorlookup.com/api/v2/%s' % mac)
-    jsonobj = json.load(resp)
-    return jsonobj[0]['company']
+def log_messages(level, message):
+    log(0, (MESSAGE_LEVELS[level], message))
+
+def log_probes(data):
+    log(1, data)
+
+def resolve_oui(mac):
+    if mac not in ouis:
+        try:
+            #resp = urllib2.urlopen('https://www.macvendorlookup.com/api/v2/%s' % mac)
+            #jsonobj = json.load(resp)
+            #ouis[mac] = jsonobj[0]['company']
+            resp = urllib2.urlopen('http://api.macvendors.com/%s' % mac)
+            ouis[mac] = resp.read()
+            log_messages(0, 'OUI resolved. [%s => %s]' % (mac, ouis[mac]))
+        except Exception as e:
+            log_messages(1, 'OUI resolution failed. [%s => %s]' % (mac, str(e)))
+            return 'Unknown'
+    return ouis[mac]
 
 def call_alerts(**kwargs):
     for var in globals():
@@ -38,10 +60,10 @@ def call_alerts(**kwargs):
                 func = globals()[var.lower()]
                 try:
                     func(**kwargs)
-                    log(0, '%s alert triggered [%s]' % (var[6:], kwargs['bssid']))
+                    log_messages(0, '%s alert triggered. [%s]' % (var[6:], kwargs['bssid']))
                 except:
                     traceback.print_exc()
-                    log(1, '%s alert failed [%s]' % (var[6:], kwargs['bssid']))
+                    log_messages(1, '%s alert failed. [%s]' % (var[6:], kwargs['bssid']))
 
 def packet_handler(pkt):
     rtlen = struct.unpack('h', pkt[2:4])[0]
@@ -52,38 +74,54 @@ def packet_handler(pkt):
         # parse bssid
         bssid = pkt[36:42].encode('hex')
         bssid = ':'.join([bssid[x:x+2] for x in xrange(0, len(bssid), 2)])
+        # parse rssi
+        rssi = struct.unpack("b",pkt[:rtlen][-4:-3])[0]
+        # parse essid
+        essid = pkt[52:52+ord(pkt[51])] if ord(pkt[51]) > 0 else '<None>'
+        # get oui for bssid
+        oui = resolve_oui(bssid)
+        # build data tuple
+        data = (bssid, rssi, essid, oui)
         # check whitelist for probing mac address
+        foreign = False
         if bssid not in MAC_LIST:
-            # parse rssi
-            rssi = struct.unpack("b",pkt[:rtlen][-4:-3])[0]
-            # check proximity
-            if rssi > RSSI_THRESHOLD:
-                # parse essid
-                essid = pkt[52:52+ord(pkt[51])] if ord(pkt[51]) > 0 else '<None>'
-                # get oui for bssid
-                if bssid not in ouis:
-                    try: ouis[bssid] = oui_lookup(bssid)
-                    except: ouis[bssid] = 'Unknown'
-                    log(0, 'OUI resolved [%s => %s]' % (bssid, ouis[bssid]))
-                oui = ouis[bssid]
-                log(2, 'Foreign probe detected [MAC=%s, RSSI=%d, SSID=%s, OUI=%s]' % (bssid, rssi, essid, oui))
-                # send alerts periodically
-                if bssid not in alerts:
-                    alerts[bssid] = datetime.now() - timedelta(minutes=5)
-                if (datetime.now() - alerts[bssid]).seconds > ALERT_THRESHOLD:
-                    alerts[bssid] = datetime.now()
-                    call_alerts(bssid=bssid, rssi=rssi, essid=essid, oui=oui)
+            foreign = True
+        # check proximity
+        on_premises = False
+        if rssi > RSSI_THRESHOLD:
+            on_premises = True
+        # log according to configured level
+        if LOG_LEVEL == 0: log_probes(data)
+        if foreign and LOG_LEVEL == 1: log_probes(data)
+        if on_premises and LOG_LEVEL == 2: log_probes(data)
+        if foreign and on_premises:
+            if LOG_LEVEL == 3: log_probes(data)
+            # send alerts periodically
+            if bssid not in alerts:
+                alerts[bssid] = datetime.now() - timedelta(minutes=5)
+            if (datetime.now() - alerts[bssid]).seconds > ALERT_THRESHOLD:
+                if LOG_LEVEL == 4: log_probes(data)
+                alerts[bssid] = datetime.now()
+                call_alerts(bssid=bssid, rssi=rssi, essid=essid, oui=oui)
 
-log(0, 'WUDS started')
-# setup the sniffer
-rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
-rawSocket.bind((IFACE, 0x0003))
-alerts = {}
-ouis = {}
-# start the sniffer
-while True:
-    try:
-        pkt = rawSocket.recvfrom(2048)[0]
-        packet_handler(pkt)
-    except KeyboardInterrupt: break
-log(0, 'WUDS exited')
+# connect to the wuds database
+# wuds runs as root and should be able to write anywhere
+with sqlite3.connect(LOG_FILE) as conn:
+    with closing(conn.cursor()) as cur:
+        # build the database schema if necessary
+        cur.execute('CREATE TABLE IF NOT EXISTS probes (dtg TEXT, mac TEXT, rssi INT, ssid TEXT, oui TEXT)')
+        cur.execute('CREATE TABLE IF NOT EXISTS messages (dtg TEXT, lvl TEXT, msg TEXT)')
+        conn.commit()
+        log_messages(0, 'WUDS started.')
+        # set up the sniffer
+        rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
+        rawSocket.bind((IFACE, 0x0003))
+        alerts = {}
+        ouis = {}
+        # start the sniffer
+        while True:
+            try:
+                pkt = rawSocket.recvfrom(2048)[0]
+                packet_handler(pkt)
+            except KeyboardInterrupt: break
+        log_messages(0, 'WUDS stopped.')

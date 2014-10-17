a8f5c04aecf3d46e23a16fe74695738e94611b0f
LaNMaSteR53 <tjt1980@gmail.com>
2014-10-16 23:24:52 -0400

    Fixed a bug in the resolve_oui function.

--- core.py
+++ core.py
@@ -54,24 +54,27 @@ def is_admin_oui(mac):
     return int(mac.split(':')[0], 16) & 2
 
 def resolve_oui(mac):
+    # check if mac vendor has already been resolved
     if mac not in ouis:
-        try:
-            resp = urllib2.urlopen('https://www.macvendorlookup.com/api/v2/%s' % mac)
-            if resp.code == 204:
-                if is_admin_oui:
-                    ouis[mac] = ADMIN_OUI
-                else:
+        # check if mac has a local admin oui
+        if is_admin_oui(mac):
+            ouis[mac] = ADMIN_OUI
+        # retrieve mac vendor from oui lookup api
+        else:
+            try:
+                resp = urllib2.urlopen('https://www.macvendorlookup.com/api/v2/%s' % mac)
+                if resp.code == 204:
                     ouis[mac] = 'Unknown'
-            elif resp.code == 200:
-                jsonobj = json.load(resp)
-                ouis[mac] = jsonobj[0]['company']
-            else:
-                raise Exception('Invalid response code: %d' % (resp.code))
-            log_message(0, 'OUI resolved. [%s => %s]' % (mac, ouis[mac]))
-        except Exception as e:
-            log_message(1, 'OUI resolution failed. [%s => %s]' % (mac, str(e)))
-            # return, but don't store the value
-            return 'Error'
+                elif resp.code == 200:
+                    jsonobj = json.load(resp)
+                    ouis[mac] = jsonobj[0]['company']
+                else:
+                    raise Exception('Invalid response code: %d' % (resp.code))
+                log_message(0, 'OUI resolved. [%s => %s]' % (mac, ouis[mac]))
+            except Exception as e:
+                log_message(1, 'OUI resolution failed. [%s => %s]' % (mac, str(e)))
+                # return, but don't store the value
+                return 'Error'
     return ouis[mac]
 
 def call_alerts(**kwargs):
